d40eb696578a1c26ed64f3e6591c5f3292123d41
AutisticKangaroo <autistickangaroo@protonmail.com>
2021-07-28 16:30:55 +1200

    add files

---
+++ include/dipc/dipc.hpp
@@ -0,0 +1,36 @@
+#ifndef _DIPC_DIPC_HPP_
+#define _DIPC_DIPC_HPP_
+
+#include <cstdint>
+
+#ifdef __GNUC__
+#define DIPC_DISABLE_PACK( __Declaration__ ) __Declaration__ __attribute__((__packed__))
+#endif
+
+#ifdef _MSC_VER
+#define DIPC_DISABLE_PACK(__Declaration__) __pragma( pack(push, 1) ) __Declaration__ __pragma( pack(pop))
+#endif
+
+#define DIPC_FRAMEBUFFER_SIZE(width, height) (4 * width * height)
+
+DIPC_DISABLE_PACK(struct dipc_header {
+    uint32_t unk;
+    uint64_t frame_time;
+    uint32_t width;
+    uint32_t height;
+});
+
+namespace dipc {
+    struct connection {
+        uint32_t pid;
+        void* handle;
+        uint8_t* mapping;
+    };
+
+    bool connect(uint32_t pid, connection& connection);
+    void disconnect(const connection& connection);
+
+    void commit_frame(uint8_t* mapping, uint8_t* frame, size_t width, size_t height);
+}
+
+#endif
---
+++ src/dipc.cpp
@@ -0,0 +1,75 @@
+#include <dipc/dipc.hpp>
+
+#include <iostream>
+#include <Windows.h>
+
+#define DIPC_SHARED_MEMORY_BUFFER_SIZE 0x3200014u
+
+namespace dipc {
+    bool connect(uint32_t pid, connection& connection) {
+        const char format[] = "DiscordOverlay_Framebuffer_Memory_%d";
+
+        int32_t size = snprintf(nullptr, 0, format, pid) + 1;
+
+        if (size == -1)
+            return false;
+
+        std::string object;
+
+        object.resize(size);
+
+        snprintf((char*) object.data(), size, format, pid);
+
+        HANDLE handle = OpenFileMappingA(
+            FILE_MAP_READ | FILE_MAP_WRITE,
+            FALSE,
+            object.data()
+        );
+
+        do {
+            if (handle == nullptr)
+                break;
+
+            auto mapping = (uint8_t*) MapViewOfFile(
+                handle,
+                FILE_MAP_READ | FILE_MAP_WRITE,
+                0,
+                0,
+                DIPC_SHARED_MEMORY_BUFFER_SIZE
+            );
+
+            if (mapping == nullptr)
+                break;
+
+            connection.pid = pid;
+            connection.handle = handle;
+            connection.mapping = mapping;
+
+            return true;
+        } while (false);
+
+        if (handle != nullptr)
+            CloseHandle(handle);
+
+        return false;
+    }
+
+    void disconnect(const connection& connection) {
+        UnmapViewOfFile(connection.mapping);
+        CloseHandle(connection.mapping);
+    }
+
+    // RGBA only
+    void commit_frame(uint8_t* mapping, uint8_t* frame, size_t width, size_t height) {
+        auto header = (dipc_header*) mapping;
+
+        const size_t size = DIPC_FRAMEBUFFER_SIZE(width, height);
+
+        memcpy(mapping + sizeof(dipc_header), frame, size);
+
+        header->width = width;
+        header->height = height;
+
+        ++header->frame_time;
+    }
+}
